74331d9e8cbe6d91618e14a79ee5ff38d7c0e5b6
Zach Quinn <zacharytq@gmail.com>
2021-09-04 17:40:51 -0400

    rendered spinning box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { useRef } from 'react';
 import './App.scss';
-import { Canvas } from '@react-three/fiber';
-import { Box } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach='geometry' args={[1,1,1]} />
+      <meshStandardMaterial attach='material' />
+    </mesh>
+  )
+}
 
 
 function App() {
   return (
     <>
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach='geometry' args={[1,1,1]} />
-          <meshStandardMaterial attach='material' />
-        </mesh>
+        <Box />
       </Canvas>
     </>
   );
