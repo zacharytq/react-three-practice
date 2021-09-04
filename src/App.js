2dda780c208409bb1bb630c5157a2d6a8830b3df
Zach Quinn <zacharytq@gmail.com>
2021-09-04 19:20:50 -0400

    added position props to Box, rendered three times

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.scss';
 import { Canvas, useFrame } from '@react-three/fiber';
 
 
-const Box = () => {
+const Box = ({position}) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
-    <mesh ref={mesh}>
+    <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach='geometry' args={[1,1,1]} />
       <meshStandardMaterial attach='material' color='blue'/>
     </mesh>
@@ -20,7 +20,9 @@ function App() {
     <>
       <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
         <ambientLight intensity={0.3} />
-        <Box />
+        <Box position={[0, 1, 0]} />
+        <Box position={[-2, 1, -5]} />
+        <Box position={[5, 1, -2]} />
       </Canvas>
     </>
   );
