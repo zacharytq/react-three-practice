4b752e5e48d9c9cba8ca505812b4b78c1ec81940
Zach Quinn <zacharytq@gmail.com>
2021-09-04 17:27:25 -0400

    rendered a box in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Canvas } from '@react-three/fiber';
+import { Box } from '@react-three/drei';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas>
+        <mesh>
+          <boxBufferGeometry attach='geometry' args={[1,1,1]} />
+          <meshStandardMaterial attach='material' />
+        </mesh>
+      </Canvas>
+    </>
   );
 }
 
