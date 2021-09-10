0baeb342a44965d8ef23ea916cd04d4486decb9a
Zach Quinn <zacharytq@gmail.com>
2021-09-10 11:32:03 -0400

    trying to get shadows to work

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const Box = ({position, color}) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
-    <mesh position={position} ref={mesh}>
+    <mesh castShadow position={position} ref={mesh}>
       <boxBufferGeometry attach='geometry' args={[1,1,1]} />
       <meshStandardMaterial attach='material' color={color}/>
     </mesh>
@@ -18,26 +18,27 @@ const Box = ({position, color}) => {
 function App() {
   return (
     <>
-      <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+      <Canvas shadowMap colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
         <ambientLight intensity={0.3} />
         <directionalLight 
+          castShadow
           position={[0, 10, 0]}
           intensity={1}
-          shadowMapWidth={1024}
-          shadowMapHeight={1024}
-          shadowCameraFar={50}
-          shadowCameraLeft={-10}
-          shadowCameraRight={10}
-          shadowCameraTop={10}
-          shadowCameraBottom={-10}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
         />
         <pointLight position={[-10, 0, -20]} intensity={0.5} />
         <pointLight position={[0, -10, 0]} intensity={1.5} />
 
         <group>
-          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
             <planeBufferGeometry attach='geometry' args={[100,100]} />
-            <meshStandardMaterial attach='material' color='yellow' />
+            <shadowMaterial attach='material' opacity={0.3} color='white'/>
           </mesh>
         </group>
 
