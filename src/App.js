0c89d4a861a9e762a4b1607f1f7ba1a19b6291b6
Zach Quinn <zacharytq@gmail.com>
2021-09-10 10:51:01 -0400

    added color props to Box component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.scss';
 import { Canvas, useFrame } from '@react-three/fiber';
 
 
-const Box = ({position}) => {
+const Box = ({position, color}) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
   return (
     <mesh position={position} ref={mesh}>
       <boxBufferGeometry attach='geometry' args={[1,1,1]} />
-      <meshStandardMaterial attach='material' color='blue'/>
+      <meshStandardMaterial attach='material' color={color}/>
     </mesh>
   )
 }
@@ -20,9 +20,9 @@ function App() {
     <>
       <Canvas colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
         <ambientLight intensity={0.3} />
-        <Box position={[0, 1, 0]} />
-        <Box position={[-2, 1, -5]} />
-        <Box position={[5, 1, -2]} />
+        <Box position={[0, 1, 0]} color='pink' />
+        <Box position={[-2, 1, -5]} color='lightblue'/>
+        <Box position={[5, 1, -2]} color='red'/>
       </Canvas>
     </>
   );
