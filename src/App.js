c103314baeca4de27afdb44456afb7cff13df264
Zach Quinn <zacharytq@gmail.com>
2021-09-12 20:51:27 -0400

    shadows working

--- src/App.js
+++ src/App.js
@@ -18,12 +18,11 @@ const Box = ({position, color}) => {
 function App() {
   return (
     <>
-      <Canvas shadowMap colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
+      <Canvas shadows colorManagement camera={{position: [-5, 2, 10], fov: 60}}>
         <ambientLight intensity={0.3} />
         <directionalLight 
-          castShadow
-          position={[0, 10, 0]}
           intensity={1}
+          castShadow
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
           shadow-camera-far={50}
@@ -31,14 +30,15 @@ function App() {
           shadow-camera-right={10}
           shadow-camera-top={10}
           shadow-camera-bottom={-10}
+          position={[0, 10, 0]}
         />
         <pointLight position={[-10, 0, -20]} intensity={0.5} />
         <pointLight position={[0, -10, 0]} intensity={1.5} />
 
         <group>
-          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]} args={[1000, 1000]} receiveShadow >
             <planeBufferGeometry attach='geometry' args={[100,100]} />
-            <shadowMaterial attach='material' opacity={0.3} color='white'/>
+            <shadowMaterial attach='material' opacity={0.3} />
           </mesh>
         </group>
 
